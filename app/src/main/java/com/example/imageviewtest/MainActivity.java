826662efdebb9e3e184c5eb7b8c1479d11a85d7c
JPep26 <94396777+JPep26@users.noreply.github.com>
2021-11-23 17:56:37 +0900

    MINE

--- app/src/main/java/com/example/imageviewtest/MainActivity.java
+++ app/src/main/java/com/example/imageviewtest/MainActivity.java
@@ -1,59 +1,128 @@
 package com.example.imageviewtest;
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
 
 import com.bumptech.glide.Glide;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+import java.io.File;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity
-{
-    private final String TAG = this.getClass().getSimpleName();
-
-    ImageView imageview;
-
+public class MainActivity extends AppCompatActivity {
+    ImageView UploadPic;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageview = findViewById(R.id.imageview);
 
-        imageview.setOnClickListener(v -> {
-            Intent intent = new Intent();
-            intent.setType("image/*");
-            intent.setAction(Intent.ACTION_GET_CONTENT);
-            launcher.launch(intent);
-        });
+        //동적퍼미션 작업
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+
+            int permissionResult= checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+            if(permissionResult== PackageManager.PERMISSION_DENIED){
+                String[] permissions= new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permissions,10);
+            }
+        }else{
+            //cv.setVisibility(View.VISIBLE);
+        }
+
+        //갤러리 or 사진 앱 실행하여 사진을 선택하도록
+        Intent intent= new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,10);
+
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 10 :
+                if(grantResults[0]==PackageManager.PERMISSION_GRANTED) //사용자가 허가 했다면
+                {
+                    Toast.makeText(this, "외부 메모리 읽기/쓰기 권한을 허용하였습니다.", Toast.LENGTH_SHORT).show();
+
+                }else{//거부했다면
+                    Toast.makeText(this, "외부 메모리 읽기/쓰기 권한을 제한하였습니다.", Toast.LENGTH_SHORT).show();
+
+                }
+                break;
+        }
+
     }
 
-    ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>()
-            {
-                @Override
-                public void onActivityResult(ActivityResult result)
-                {
-                    if (result.getResultCode() == RESULT_OK)
-                    {
-                        Log.e(TAG, "result : " + result);
-                        Intent intent = result.getData();
-                        Log.e(TAG, "intent : " + intent);
-                        Uri uri = intent.getData();
-                        Log.e(TAG, "uri : " + uri);
-//                        imageview.setImageURI(uri);
-                        Glide.with(MainActivity.this)
-                                .load(uri)
-                                .into(imageview);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case 10:
+                if(resultCode==RESULT_OK){
+                    //선택한 사진의 경로(Uri)객체 얻어오기
+                    Uri uri= data.getData();
+                    if(uri!=null){
+                        UploadPic.setImageURI(uri);
+
+                        //갤러리앱에서 관리하는 DB정보가 있는데, 그것이 나온다 [실제 파일 경로가 아님!!]
+                        //얻어온 Uri는 Gallery앱의 DB번호임. (content://-----/2854)
+                        //업로드를 하려면 이미지의 절대경로(실제 경로: file:// -------/aaa.png 이런식)가 필요함
+                        //Uri -->절대경로(String)로 변환
+                        String imgPath= getRealPathFromUri(uri);   //임의로 만든 메소드 (절대경로를 가져오는 메소드)
+
+                        //이미지 경로 uri 확인해보기
+                        //new AlertDialog.Builder(this).setMessage(uri.toString()+"\n"+imgPath).create().show();
+                        Log.d("이미지 경로",uri.toString()+"\n"+imgPath);
+
                     }
+
+                }else
+                {
+                    Toast.makeText(this, "이미지가 선택되지 않았습니다.", Toast.LENGTH_SHORT).show();
                 }
-            });
+                break;
+        }
+    }//onActivityResult()
+
+    //Uri -- > 절대경로로 바꿔서 리턴시켜주는 메소드
+    String getRealPathFromUri(Uri uri){
+        String[] proj= {MediaStore.Images.Media.DATA};
+        CursorLoader loader= new CursorLoader(this, uri, proj, null, null, null);
+        Cursor cursor= loader.loadInBackground();
+        int column_index= cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result= cursor.getString(column_index);
+        cursor.close();
+        return  result;
+    }
+
+
+
 
 }
